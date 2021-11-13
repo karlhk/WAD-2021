1d082a3562fa618e6ebcaca51a58bcc72f3e7239
Kalmer <kalmerkaurson@gmail.com>
2021-11-13 15:03:37 +0200

    footer, header, login fixes

--- res/js/script.js
+++ res/js/script.js
@@ -17,6 +17,24 @@ window.onclick = function(event) {
   }
 }
 
+function menuDropdownFunction() {
+  document.getElementById("menuDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn2')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 function addPostsToMainBlock(blockData) {
     //alert(blockData.length);
     //alert(blockData[0].userImage);
